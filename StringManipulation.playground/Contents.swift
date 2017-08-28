f22405b0ce022adc0fdcf899d96a90996eaa6b2b
Daniel No <mr.danielno@gmail.com>
2017-08-28 16:22:01 -0400

    terrible substrings

--- StringManipulation.playground/Contents.swift
+++ StringManipulation.playground/Contents.swift
@@ -2,7 +2,7 @@
 
 import UIKit
 
-// Reverse a string
+// Reverse a string with character array or use .reverse function
 
 let str = "Hello World"
 
@@ -23,5 +23,22 @@ let reversed = reverseString(string: str)
 let index = str.index(str.startIndex, offsetBy: 5)
 let hello = str.substring(to: index)  // Hello
 print(hello)
+
+
+// Get the nth character in string
+
+let nthIndex = str.index(str.startIndex, offsetBy: 6)
+let nthChar = str[nthIndex]
+print(nthChar)
+
+// Make a string range to substring
+let startIndex = str.index(str.startIndex, offsetBy: 5)
+let endIndex = str.index(str.startIndex, offsetBy: 11)
+let range = startIndex ..< endIndex
+let world = str.substring(with: range)
+
+// Substring using character array
+let helloString = String(str.characters.prefix(5))
+let worldString = String(str.characters.suffix(6))
 
 
