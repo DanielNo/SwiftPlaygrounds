cd4f82bf2612c1b39489492e213c8cbe5b59bd69
Daniel No <mr.danielno@gmail.com>
2020-03-31 16:43:09 -0400

    practicing basics

--- Loops.playground/Contents.swift
+++ Loops.playground/Contents.swift
@@ -6,22 +6,30 @@ var nums = [1,2,3,4]
 
 // For loop with Range
 for num in 0...2{
-print("hello")
 }
 
 for num in 0..<10{
-print(num)
 }
 
 // For loop with index and element
 for (index,element) in nums.enumerated(){
-    print("\(index) \(element)")
+//    print("\(index) \(element)")
 }
 
 
 let names = ["john","mary","paul","wendy"]
 
 for name in names{
-    print("welcome \(name)!")
+//    print("welcome \(name)!")
 }
 
+func compareTwoIndices(_ arr : [Int]){
+    
+    for i in 0..<arr.count-1{
+        print("\(arr[i]) vs \(arr[i+1])")
+    }
+    
+}
+compareTwoIndices(nums)
+
+let str : [Character] = Array(repeating: "a", count: 500)
