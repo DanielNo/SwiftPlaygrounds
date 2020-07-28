ee89c6e8b3a009cc41ea048492d9c6fcfe994edc
Daniel No <mr.danielno@gmail.com>
2020-07-28 19:22:59 -0400

    heap skeleton

---
+++ Heap.playground/Contents.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+public class MaxHeap{
+    var arr : [Int] = []
+    
+    
+    public init(_ root : Int) {
+        arr.insert(root, at: 0)
+    }
+    
+    public init(_ nums : [Int]){
+        arr = nums
+    }
+    
+    public func insert(val : Int){
+        
+    }
+    
+    // Should return top element and restructure the heap
+    public func poll() -> Void{
+        
+    }
+    
+    
+}
+
+MaxHeap(20)
